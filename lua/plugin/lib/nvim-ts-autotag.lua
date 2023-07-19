089389bbddd13b3589d1d87f8346f2e920458fbe
Jemal Rashidi <jmsrsd@gmail.com>
2023-07-19 09:29:11 +0700

    revert autotag and web-devicons config

--- lua/plugin/lib/nvim-ts-autotag.lua
+++ lua/plugin/lib/nvim-ts-autotag.lua
@@ -1,5 +1,6 @@
 return {
   'windwp/nvim-ts-autotag',
-  event = "InsertEnter",
-  opts = {},
+  config = function()
+    require('nvim-ts-autotag').setup()
+  end,
 }
--- lua/plugin/lib/nvim-web-devicons.lua
+++ lua/plugin/lib/nvim-web-devicons.lua
@@ -1,4 +1,6 @@
 return {
   'nvim-tree/nvim-web-devicons',
-  opts = {},
+  config = function()
+    require("nvim-web-devicons").setup()
+  end,
 }
