e2bc52ba320684139c9552b52848b7bde500b156
Jemal Rashidi <jmsrsd@gmail.com>
2023-07-20 14:41:18 +0700

    add xo null-ls extensons

--- lua/plugin/lib/null-ls/config.lua
+++ lua/plugin/lib/null-ls/config.lua
@@ -5,6 +5,10 @@ M.setup = function()
 
   null_ls.setup({
     on_attach = require('plugin.lib.lsp-zero.keymap'),
+    sources = {
+      null_ls.builtins.code_actions.xo,
+      null_ls.builtins.diagnostics.xo,
+    }
   })
 end
 
