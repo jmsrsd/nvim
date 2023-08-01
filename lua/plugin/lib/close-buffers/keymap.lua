2cddd6d3183be49256ac2df04ffa36e4da2e35fd
Jemal Rashidi <jmsrsd@gmail.com>
2023-08-01 12:57:20 +0700

    suppress close-buffers plugin error messages

--- lua/plugin/lib/close-buffers/keymap.lua
+++ lua/plugin/lib/close-buffers/keymap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '<leader>qq', function()
   pcall(function() vim.cmd('exe "normal \\<CR>"') end)
-  vim.cmd('NvimTreeOpen')
-  require('close_buffers').wipe({ type = 'other' })
+  pcall(function() vim.cmd('NvimTreeOpen') end)
+  pcall(function() require('close_buffers').wipe({ type = 'other' }) end)
 end, {
   noremap = true,
   desc = 'Delete all buffers except the current focused (Nvim Tree)',
