78f8d7dbf4f5f07c3da3c49c28590225ebe1bf32
Jemal Rashidi <jmsrsd@gmail.com>
2023-08-14 08:18:14 +0700

    set null-ls root directory in order to on the same level as package.json

--- lua/plugin/lib/null-ls/config.lua
+++ lua/plugin/lib/null-ls/config.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.setup = function()
   local on_attach = require('plugin.lib.lsp-zero.keymap')
-  local root_dir = vim.loop.cwd
+  local root_dir = require('null-ls.utils').root_pattern("package.json")
+  -- local root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git")
+  -- local root_dir = vim.loop.cwd
 
   require("null-ls").setup({
     -- update_in_insert = true,
