9a3fc1c192644d968def6bdc113a2b77d2eeae67
Jemal Rashidi <jmsrsd@gmail.com>
2023-08-21 14:30:40 +0700

    change neovide font size to 10

--- lua/config/option/neovide.lua
+++ lua/config/option/neovide.lua
@@ -1,12 +1,12 @@
 -- Font styles
 --
-vim.o.guifont = 'JetBrainsMono Nerd Font Mono:h12'
+vim.o.guifont = 'JetBrainsMono Nerd Font Mono:h10'
 
 -- Padding
 --
 local horizontal_padding = 0
 
-vim.g.neovide_padding_top = 24
+vim.g.neovide_padding_top = 20
 vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = horizontal_padding
 vim.g.neovide_padding_left = horizontal_padding
