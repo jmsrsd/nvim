e131c49817c9cba67a381c37ae07dad4267711a5
Jemal Rashidi <jmsrsd@gmail.com>
2023-08-31 15:59:08 +0700

    change cmp confirm mapping from <CR> to <Tab>

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -38,7 +38,7 @@ return {
 				["<C-e>"] = cmp.mapping.abort(),
 				-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				--
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
