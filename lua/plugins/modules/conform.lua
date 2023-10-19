5d450ae4e07e0739f64b43ae292b2317457ab339
Jemal Rashidi <jmsrsd@pop-os.localdomain>
2023-10-19 12:12:28 +0700

    chore: update conform.lua and .luarc.json
    - Update conform.lua to include new formatters for various file types.
    - Also, remove trailing space in .luarc.json.

--- lua/plugins/modules/conform.lua
+++ lua/plugins/modules/conform.lua
@@ -17,7 +17,26 @@ return {
     conform.setup {
       --- These options will be passed to conform.format()
       ---
-      format_on_save = format_opts
+      format_on_save = format_opts,
+
+      formatters_by_ft = {
+
+        json = { 'prettierd' },
+
+        javascript = { 'prettierd' },
+
+        jsx = { 'prettierd' },
+
+        typescript = { 'prettierd' },
+
+        tsx = { 'prettierd' },
+
+        html = { 'prettierd' },
+
+        css = { 'prettierd' },
+
+        yaml = { 'prettierd' },
+      },
     }
 
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
