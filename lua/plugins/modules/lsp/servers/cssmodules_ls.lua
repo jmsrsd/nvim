32fe96f9a0f07e427fd98d780be8874b0f512a1a
Jemal Rashidi <jmsrsd@gmail.com>
2023-10-25 20:40:12 +0700

    feat(lsp): add snippet support for LSP servers

--- lua/plugins/modules/lsp/servers/cssmodules_ls.lua
+++ lua/plugins/modules/lsp/servers/cssmodules_ls.lua
@@ -1,3 +1,6 @@
+---@diagnostic disable: inject-field
+---
+
 --- Language:
 ---
 --- CSS
@@ -12,6 +15,10 @@ local lspconfig = require("lspconfig")
 local _, capabilities = pcall(function()
 	return require("cmp_nvim_lsp").default_capabilities()
 end)
+
+capabilities = capabilities or vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.cssmodules_ls.setup({
 	capabilities = capabilities,
