221201e9897bfe154d3ce91932d7f6a11fa7eaf0
Jemal Rashidi <jmsrsd@gmail.com>
2023-11-20 14:16:10 +0700

    feat(catppuccin): add new styles and integrations
    - This commit adds new styles for general hi groups, including comments,
      conditionals, loops, keywords, types, functions, strings, variables,
      numbers, booleans, properties, and operators.
    - It also adds new integrations for notify, aerial, alpha, cmp,
      dashboard, flash, gitsigns, headlines, illuminate, indent_blankline,
      leap, lsp_trouble, mason, markdown, mini, native_lsp, navic, neotest,
      neotree, noice, semantic_tokens, telescope, and treesitter.
    - Additionally, it enables italics and 256 colors, and applies the
      colorscheme 'catppuccin'.

--- lua/jmsrsd/configs/plugins/modules/catppuccin.lua
+++ lua/jmsrsd/configs/plugins/modules/catppuccin.lua
@@ -17,6 +17,33 @@ return {
 
 			transparent_background = true,
 
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+
+				comments = { "italic" }, -- Change the style of comments
+
+				conditionals = { "italic" },
+
+				loops = { "italic" },
+
+				keywords = { "italic" },
+
+				types = { "italic" },
+
+				functions = {},
+
+				strings = {},
+
+				variables = {},
+
+				numbers = {},
+
+				booleans = {},
+
+				properties = {},
+
+				operators = {},
+			},
+
 			integrations = {
 
 				notify = true,
@@ -26,9 +53,78 @@ return {
 				treesitter_context = true,
 
 				which_key = true,
+
+				aerial = true,
+
+				alpha = true,
+
+				cmp = true,
+
+				dashboard = true,
+
+				flash = true,
+
+				gitsigns = true,
+
+				headlines = true,
+
+				illuminate = true,
+
+				indent_blankline = { enabled = true },
+
+				leap = true,
+
+				lsp_trouble = true,
+
+				mason = true,
+
+				markdown = true,
+
+				mini = true,
+
+				native_lsp = {
+
+					enabled = true,
+
+					underlines = {
+
+						errors = { "undercurl" },
+
+						hints = { "undercurl" },
+
+						warnings = { "undercurl" },
+
+						information = { "undercurl" },
+					},
+				},
+
+				navic = { enabled = true, custom_bg = "lualine" },
+
+				neotest = true,
+
+				neotree = true,
+
+				noice = true,
+
+				semantic_tokens = true,
+
+				telescope = true,
+
+				treesitter = true,
 			},
 		})
 
+		--- Enable italics
+		---
+		vim.cmd("set t_ZH=^[[3m")
+		vim.cmd("set t_ZR=^[[23m")
+
+		--- Enable 256 colors
+		---
+		vim.o.termguicolors = true
+
+		--- Apply colorscheme
+		---
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
