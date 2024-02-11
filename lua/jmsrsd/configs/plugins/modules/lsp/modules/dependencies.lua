5d786ae6548727e0bdab9fd3b04cee0ea69fd48a
Jemal Rashidi <jmsrsd@gmail.com>
2024-02-11 19:47:14 +0700

    feat: Update lazy-lock.json and dependencies.lua
    - Update lazy-lock.json with new lsp_signature.nvim commit
    - Update dependencies.lua with new lsp_signature.nvim configuration

--- lua/jmsrsd/configs/plugins/modules/lsp/modules/dependencies.lua
+++ lua/jmsrsd/configs/plugins/modules/lsp/modules/dependencies.lua
@@ -7,6 +7,22 @@ return {
 
 	"Hoffs/omnisharp-extended-lsp.nvim",
 
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+		},
+		config = function(_, opts)
+			local signature = require("lsp_signature")
+
+			signature.setup(opts)
+		end,
+	},
+
 	--- Flutter
 	---
 	"nvim-lua/plenary.nvim",
