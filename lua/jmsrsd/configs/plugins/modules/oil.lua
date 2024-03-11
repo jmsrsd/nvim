79cdeb55ceb10cd344411c3fe61ce9bcb6b3013e
Jemal Rashidi <jmsrsd@gmail.com>
2024-03-12 05:32:18 +0700

    refactor: update method call syntax in oil.lua

--- lua/jmsrsd/configs/plugins/modules/oil.lua
+++ lua/jmsrsd/configs/plugins/modules/oil.lua
@@ -1,7 +1,7 @@
 local path = require("jmsrsd.utils.path") --[[@as Path]]
 
 local import = function(module)
-	return path:import(function(parent)
+	return path.import(function(parent)
 		return parent .. "/" .. module
 	end, function() end)
 end
