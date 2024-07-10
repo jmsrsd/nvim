dad5be68f13701f9fe014b879f8018f86266af2a
Jemal Rashidi <jmsrsd@gmail.com>
2024-07-11 06:59:50 +0700

    refactor: refactor module imports in init.lua
    Simplified module import handling by using a higher-order function to
    import modules from the './configs/' directory in init.lua.

--- lua/jmsrsd/init.lua
+++ lua/jmsrsd/init.lua
@@ -2,18 +2,10 @@ local context = require("jmsrsd.commons.context").new({
 	target = function() end,
 })
 
-local import = context.import
-
-local modules = {
+vim.tbl_map(function(module)
+	return context.import("./configs/" .. module)
+end, {
 	"options",
 	"keymaps",
 	"plugins",
-}
-
-local prepend = function(module)
-	return "./configs/" .. module
-end
-
-modules = vim.tbl_map(prepend, modules)
-
-modules = vim.tbl_map(import, modules)
+})
